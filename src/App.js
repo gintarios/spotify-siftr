8ad9fdf41f16ff4a26208bcc9e64a98f4141cf1b
Gintare <37443286+gintarios@users.noreply.github.com>
2018-12-05 17:10:43 +0000

    Header added #2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import 'reset-css/reset.css';
-import './App.css';
-import queryString from 'query-string';
-import genrePlaylists from './genrePlaylists';
-import Header from './frontend/views/Header'
-import SearchBox from './frontend/views/SearchBox'
+import React, { Component } from "react";
+import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+import GetTracks from "./data/GetTracks";
+import NowPlaying from "./data/NowPlaying";
+import GenresGrid from "./views/GenresGrid";
+import Header from "./views/Header";
+
+const spotifyApi = new SpotifyWebApi();
 
 function isEven(number) {
   return number % 2
@@ -35,85 +37,23 @@ class App extends Component {
       filterString: ''
     }
   }
-  componentDidMount() {
-    let parsed = queryString.parse(window.location.search);
-    let accessToken = parsed.access_token;
-    let genre = genrePlaylists.find(obj => obj.genre === "rock").playlistId;
-    if (!accessToken)
-      return;
-    fetch('https://api.spotify.com/v1/me', {
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(response => response.json())
-    .then(data => this.setState({
-      user: {
-        name: data.display_name,
-        bday: data.birthdate,
-        location: data.country,
-        email: data.email,
-        ext: data.external_urls,
-        followers: data.followers,
-        href: data.href,
-        id: data.id,
-        images: data.images,
-        subscriptions: data.product,
-        type:data.type,
-        uri: data.uri,
-      }
-    }))
 
-    // fetch('https://open.spotify.com/user/grahamhewett/playlist/2KSM2x2rFmY4GKWwRm9qRg?si=vbhRnIhfSriNejYfxd1gFQ', {
-    //   headers: {'Authorization': 'Bearer ' + accessToken}
-    // }).then(response => response.json())
-    fetch('https://api.spotify.com/v1/playlists/2ihY1sy2Eask1kLJME0UhG/tracks', {
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(response => response.json())
-      .then(ptracks => {
-        let pnames = ptracks.items.map((song => song.track.name))
-        var randomNames = []
-        let limit = 10;
-        for(let i = 0; i < limit; i++){
-          randomNames.push(pnames[Math.floor(Math.random()*pnames.length)]);
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then(response => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url
         }
-        this.setState({rocktracks: randomNames})
-      })
-      
-    fetch('https://api.spotify.com/v1/me/playlists', {
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(response => response.json())
-    .then(playlistData => {
-      let playlists = playlistData.items
-      let trackDataPromises = playlists.map(playlist => {
-        let responsePromise = fetch(playlist.tracks.href, {
-          headers: {'Authorization': 'Bearer ' + accessToken}
-        })
-        let trackDataPromise = responsePromise
-          .then(response => response.json())
-        return trackDataPromise
-      })
-      let allTracksDataPromises = 
-        Promise.all(trackDataPromises)
-      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
-        trackDatas.forEach((trackData, i) => {
-          playlists[i].trackDatas = trackData.items
-            .map(item => item.track)
-            .map(trackData => ({
-              name: trackData.name,
-              duration: trackData.duration_ms / 1000
-            }))
-        })
-        return playlists
-      })
-      return playlistsPromise
-    })
-    .then(playlists => this.setState({
-      playlists: playlists.map(item => {
-        return {
-          name: item.name,
-          //imageUrl: item.images.url, 
-          songs: item.trackDatas.slice(0,3)
-        }
-    })
-    }))
+      });
+    });
+  }
+
+  getTracks() {
+    let searchStr = "eric clapton";
+    spotifyApi.searchTracks(searchStr).then(response => {
+      console.log(`Search for ${searchStr}`, response);
+    });
   }
   render() {
     let playlistToRender = 
@@ -129,33 +69,12 @@ class App extends Component {
     return ( // if user is logged in display the code between ? and : otherwise 
       <div className="App">
         <Header />
-        {this.state.user ?
-          <div>
-            <br/>{this.state.rocktracks}
-            <br/>{this.state.user.name}
-            <br/>{this.state.user.display_name}
-            <br/>{this.state.user.birthdate}
-            <br/>{this.state.user.country}
-            <br/>{this.state.user.email}
-            {/* <br/>{this.state.user.external_urls}
-            <br/>{this.state.user.followers} */}
-            <br/>{this.state.user.href}
-            <br/>{this.state.user.id}
-            {/* <br/>{this.state.user.images} */}
-            <br/>{this.state.user.product}
-            <br/>{this.state.user.type}
-            <br/>{this.state.user.uri}
-            <h1>{this.state.user.name}'s Playlists
-          </h1>
-          {/* <PlaylistCounter playlists={playlistToRender}/>
-          <HoursCounter playlists={playlistToRender}/>
-          <Filter onTextChange={text => {
-              this.setState({filterString: text})
-            }}/> */}
-          {playlistToRender.map((playlist, i) => 
-            <Playlist playlist={playlist} index={i} />
-          )}
-          </div>
+        <a href="http://localhost:8888"> Login to Siftr </a>
+        <div>
+          <NowPlaying />
+          <GetTracks />
+          <GenresGrid />
+        </div>
            : 
           <button onClick={() => {
             window.location = window.location.href.includes('localhost') 
