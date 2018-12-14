51f7e2f4f359e05d31dd6ef4a5e25bac6aaeaa80
Gintare <37443286+gintarios@users.noreply.github.com>
2018-12-14 13:58:06 +0000

    Genres grid updated (#31)
    * Buttons added
    
    * Button component added to App.js
    
    * Genres grid updated

--- src/frontend/views/GenresGrid.js
+++ src/frontend/views/GenresGrid.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import Genres from './Genres'
-import './genres-grid.css'
+import React from "react";
+import Genres from "./Genres";
+import "./genres-grid.css";
 
 function setGenreSearch(genre) {
-    return console.log(`q=genre:%22${genre}%22&type=tracks&limit=100&offset=20`)
+  return console.log(`q=genre:%22${genre}%22&type=tracks&limit=100&offset=20`);
 }
 
 const GenresGrid = () => {
-    return(
-        <div>
-            <div className='genre-container'>
-                    {Genres.map((genre) => {
-                    return (
-                        <div className='genre-item' onClick = {() => 
-                            setGenreSearch(genre.genre.toLowerCase())}>
-                            <img src={require(`./${genre.id}.jpg`)} alt ='genre'  />
-                        </div>
-                    )    
-                })}
+  return (
+    <div className = "section-genresGrid">
+      <h1> Looking for music ? </h1>
+      <div className="genre-container">
+        {Genres.map(genre => {
+          return (
+            <div
+              className="genre-item"
+              onClick={() => setGenreSearch(genre.genre.toLowerCase())}
+            >
+              <img src={require(`./${genre.id}.jpg`)} alt="genre" />
             </div>
-        </div>
-        )
-    }
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default GenresGrid;
