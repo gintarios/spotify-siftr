3527dbc8e5cfc61b95eba2fb2ffe51e75a9fab20
Graham Hewett <hewettgraham@gmail.com>
2018-12-21 12:28:59 +0000

    stars

--- src/frontend/data/StarPop.js
+++ src/frontend/data/StarPop.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faStar } from '@fortawesome/free-solid-svg-icons'
 import './starPop.css'
 
+
+
 export default function starPop(num) {
-    // let starNumber = Math.ceil((num - 50) / 10);
-    let starOn = <FontAwesomeIcon icon={faStar} color="black" className="star" />
-    
-       
-    return (<div> {starOn} </div>
-    )
+   let starNumber = Math.ceil((num - 50) / 10);
+   let starOn = <FontAwesomeIcon icon={faStar} color="black" className="star" />
+   let starOff = <FontAwesomeIcon icon={faCoffee} />
+   return (
+
+       Array.from(new Array(starNumber)).map(() => {
+           return (
+               <div className="starOn">
+                   {starOn}
+               </div>
+           )
+       })
+
+   )
 }
