fecfde51b2445dd50d3cb57a46be27bdbde99f61
Gintare <37443286+gintarios@users.noreply.github.com>
2018-12-21 12:29:21 +0000

    index js updated

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import "./index.css";
 import App from "./App";
 import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 unregister();
